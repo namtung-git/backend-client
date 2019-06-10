9ef5137b87bff3278e510811d3c7b49f229b5a4d
Pedro Silva <pedro.silva@wirepas.com>
2019-06-10 12:19:08 +0300

    read schema from yaml

--- examples/parse_kpi.py
+++ examples/parse_kpi.py
@@ -7,6 +7,7 @@ import os
 import time
 from wirepas_backend_client.tools import ParserHelper, LoggerHelper
 
+import yaml
 import json
 import pandas
 import pathlib
@@ -20,172 +21,13 @@ class KPI(object):
 
     """
 
-    # TODO: source from yaml configuration file
-    perf_stats = dict(
-        version=40,
-        name=[
-            "NWstatus",
-            "AvePerfSum",
-            "SinksAnaSum",
-            "PerfAnaSum",
-            "RouteAnaSum",
-            "BulkDataLat",
-            "McastLat",
-            "DLn2nLat",
-            "ULtotalLat",
-            "ULperHopLat",
-        ],
-    )
-
-    # TODO: source from yaml configuration file
-    schema = dict(
-        AvePerfSum=dict(
-            name="AvePerfSum",
-            block_size_node=44,
-            block_size_execution=80,
-            block_size_metadata=86,
-            header_metadata_loc=0,
-            header_element_loc=0,
-            columns_mapping=[
-                "start_time",
-                "end_time",
-                "duration_s",
-                "logic_duration_s",
-                "release",
-                "hardware",
-                "interface_speed_pps",
-                "test_case",
-                "stack_mode",
-                "nb_sinks",
-                "nw_beacon_interval_s",
-                "nb_nodes",
-                "channel_map",
-                "auto_role",
-                "ac",
-                "sync_control",
-                "ipv6_control",
-                "description",
-                "ul_nodes_per_sub_interval",
-                "n2n_nodes_per_sub_interval",
-                "mcast_nodes_per_sub_interval",
-                "sub_interval_s",
-                "random_period_s",
-                "sample_len",
-                "filename",
-                "node_logic_pins",
-                "node_logic_current_uA",
-                "ble_adv_interval_s",
-                "ble_scan_range_start",
-                "ble_scan_range_end",
-                "ble_scan_modulo",
-                "ul_pps_target",
-                "ul_achieved",
-                "qos1_ul_bytes",
-                "qos1_pps_target",
-                "qos1_miss_rate",
-                "ul_999_limit_s",
-                "qos1_ul_achieved",
-                "qos1_interval_s",
-                "diaf_interval_s",
-                "diag_pps_target",
-                "n2n_pps",
-                "hop_limit/n2n",
-                "n2n_miss_rate",
-                "n2n_999_limit_s",
-                "n2n_reTx_cnt",
-                "n2n_reTx_time_ms",
-                "offset_for_N2N_dest_address",
-                "dl_unic_pps",
-                "dl_fast_mode",
-                "rpt_cnt",
-                "hop_limit/dl",
-                "dl_ucast_miss_rate",
-                "dl bcast_pps",
-                "dl_bcast_miss_rate",
-                "dl_bytes",
-                "mcast_pps",
-                "hop_limit/mcast",
-                "mcast_tx_repeat_count",
-                "offset_for_MCAST_dest_address",
-                "mcast_group_size",
-                "bulk_pps",
-                "mcast_bulk_transfer_dest",
-                "bulk_src_node",
-                "hop_limit/bulk_data",
-                "ReTx_count_with_bulk_data",
-                "Bulk/app_interval_s",
-                "Pkts_per_bulk_interval",
-                "bulk_dest_node",
-                "2nd_source_node_ul_with_bulk_data",
-                "TxMod_with_bulkdata",
-                "qos_of_bulk_data",
-                "stack_tunings",
-                "cca_threshold",
-                "cca_disable_index",
-                "cca_window_size_ms",
-                "cca_window_freeze_after_x_attempts",
-                "unack_bcast_prob.",
-                "delay_divider_for_unack_window",
-                "cmd_start_miss_rate",
-                "cmd_start_miss",
-                "cmd_start_miss_list",
-                "cmd_end_miss_rate",
-                "cmd_end_miss_list",
-                "cmd_bulk_miss",
-                "cmd_bulk_miss_list",
-                "kpi_data",
-                "latency_kpi",
-                "latency_ul_200_ms",
-                "latency_ul_250_ms",
-                "latency_ul_1000_ms",
-                "latency_ul_5000_ms",
-                "latency_ul_60000_ms",
-                "latency_hop_ul_200_ms",
-                "latency_hop_ul_250_ms",
-                "latency_hop_ul_1000_ms",
-                "latency_hop_ul_5000_ms",
-                "latency_hop_ul_60000_ms",
-                "missed_packets",
-                "not_generated_packets",
-                "scans_h",
-                "periodic_scans_h",
-                "need_nbors_h",
-                "far_conflicts_h",
-                "rest_conflicts_h",
-                "route_chngs_h",
-                "sync_losts_h",
-                "tdma_adjusts_h",
-                "minor_adjusts_h",
-                "major_adjusts_h",
-                "conflict adjusts_h",
-                "blacklists_h",
-                "other_tdma adjusts_h",
-                "boots_h",
-                "route_loops_h",
-                "sink_chngs_h",
-                "subs_removed_h",
-                "LL_DL_fails_h",
-                "LL_UL_fails_h",
-                "scan_nbors_oom_h",
-                "other_events_h",
-                "blacklisted_chn_cnt_node",
-                "cost_255_h",
-                "next0_hop chngs_h",
-                "heads_cnt(ave)",
-                "nodes_active",
-                "nodes_active_2h",
-                "nodes_active_15min",
-                "role_chngs_h",
-                "ble_bcon_miss_rate",
-            ],
-        )
-    )
-
-    def __init__(self, data_location):
+    def __init__(self, data_location, schema_file):
         super(KPI, self).__init__()
         self.data_location = data_location
         self.df = None
         self.name = None
+        with open(schema_file) as f:
+            self.schema = yaml.load(f, Loader=yaml.FullLoader)
 
     def load(self):
         self.df = pandas.read_excel(
@@ -226,19 +68,30 @@ class KPI_Stats(KPI):
 
     """
 
-    def __init__(self, name, data_location):
-        super(KPI_Stats, self).__init__(data_location=data_location)
+    def __init__(self, name, data_location, schema_file):
+        super(KPI_Stats, self).__init__(
+            data_location=data_location, schema_file=schema_file
+        )
         self.name = name
+        try:
+            self.schema = self.schema["perf_stats"][self.name]
+        except KeyError:
+            self.schema = None
+            pass
+
         self._columns = dict()
         self._pointer = dict()
         self._nb_sections = 0
-        self._cache_name = pathlib.Path("{}.pkl".format(self.data_location))
+        self._cache_name = pathlib.Path(
+            "{}-{}.pkl".format(self.data_location, self.name)
+        )
         self._columns_mapping = dict()
 
-    def setup(self):
         self.load()
-        self.sync()
-        self.columns()
+
+        if self.schema:
+            self.sync()
+            self.columns()
 
     def load(self):
 
@@ -248,17 +101,18 @@ class KPI_Stats(KPI):
             super().load()
             self.cache()
 
-        self._nb_sections = int(
-            round(
-                (
-                    len(self.df.columns)
-                    - self.schema[self.name]["block_size_execution"]
-                    - self.schema[self.name]["block_size_node"]
+        if self.schema:
+            self._nb_sections = int(
+                round(
+                    (
+                        len(self.df.columns)
+                        - self.schema["block_size_execution"]
+                        - self.schema["block_size_node"]
+                    )
+                    / (self.schema["block_size_node"]),
+                    0,
                 )
-                / (self.schema[self.name]["block_size_node"]),
-                0,
             )
-        )
 
     def cache(self, force_overwrite=False):
         if not self._cache_name.exists():
@@ -268,15 +122,15 @@ class KPI_Stats(KPI):
     def nb_sections(self):
         return self._nb_sections
 
-    def sync(self) -> None:
+    def sync(self):
         """ Sync finds the location of the "All nodes" summary """
         self._pointer["start"] = self.df.columns.get_loc("All nodes")
         self._pointer["end"] = (
-            self._pointer["start"] + self.schema[self.name]["block_size_node"]
+            self._pointer["start"] + self.schema["block_size_node"]
         )
         self._pointer["source"] = "all"
 
-    def columns(self) -> list:
+    def columns(self):
         """
         Builds a list with the column header
 
@@ -292,8 +146,8 @@ class KPI_Stats(KPI):
                 map(
                     lambda x: x,
                     self.df.iloc[
-                        self.schema[self.name]["header_metadata_loc"],
-                        : self.schema[self.name]["block_size_metadata"],
+                        self.schema["header_metadata_loc"],
+                        : self.schema["block_size_metadata"],
                     ].index,
                 )
             )
@@ -302,7 +156,7 @@ class KPI_Stats(KPI):
                 map(
                     lambda x: x,
                     self.df.iloc[
-                        self.schema[self.name]["header_element_loc"],
+                        self.schema["header_element_loc"],
                         self._pointer["start"] : self._pointer["end"],
                     ].values,
                 )
@@ -321,13 +175,13 @@ class KPI_Stats(KPI):
             if not self._columns_mapping:
                 self._columns_mapping = dict()
                 assert len(self._columns) == len(
-                    self.schema[self.name]["columns_mapping"]
+                    self.schema["columns_mapping"]
                 )
                 n = 0
                 for c in self._columns:
-                    self._columns_mapping[c] = self.schema[self.name][
-                        "columns_mapping"
-                    ][n]
+                    self._columns_mapping[c] = self.schema["columns_mapping"][
+                        n
+                    ]
                     n += 1
 
             df.columns = self._columns
@@ -335,8 +189,7 @@ class KPI_Stats(KPI):
         except AssertionError:
             print(
                 "Mismatch in column's mapping {} x {}".format(
-                    len(self._columns),
-                    len(self.schema[self.name]["columns_mapping"]),
+                    len(self._columns), len(self.schema["columns_mapping"])
                 )
             )
         return df
@@ -349,10 +202,7 @@ class KPI_Stats(KPI):
             source = self._pointer["source"]
 
         section = self.df.iloc[
-            1:,
-            pandas.np.r_[
-                : self.schema[self.name]["block_size_metadata"], start:end
-            ],
+            1:, pandas.np.r_[: self.schema["block_size_metadata"], start:end]
         ]
         section["source"] = source
 
@@ -366,14 +216,9 @@ class KPI_Stats(KPI):
         for _ in range(0, self._nb_sections - 1, 1):
             yield self.retrieve(self._pointer["start"], self._pointer["end"])
             self._pointer["start"] = self._pointer["end"]
-            self._pointer["end"] += self.schema[self.name]["block_size_node"]
+            self._pointer["end"] += self.schema["block_size_node"]
             # TODO: retrieve node from index header
-            idx = self.df[0, self._pointer["start"]].index
-
-
-# name = ["AvePerfSum"]
-def overflow_handler(pendings):
-    print(pendings)
+            # idx = self.df[0, self._pointer["start"]].index
 
 
 if __name__ == "__main__":
@@ -398,13 +243,80 @@ if __name__ == "__main__":
     )
     logger = log.setup()
 
-    filepath = pathlib.Path("./kpi_data/perf_stats_small.xlsx")
-    name = "AvePerfSum"
+    filepath = pathlib.Path("./kpi_data/perf_stats.xlsx")
 
-    kpi_stats = KPI_Stats(name=name, data_location=filepath)
-    kpi_stats.setup()
-
-    section = kpi_stats.retrieve()
-    df = section.apply(
-        lambda x: logger.info(json.loads(x.to_json(date_format="iso"))), axis=1
+    kpi = dict()
+    name = "NWstatus"
+    kpi[name] = KPI_Stats(
+        name=name,
+        data_location=filepath,
+        schema_file="./examples/test_kpi_definitions.yaml",
     )
+
+    name = "AvePerfSum"
+    kpi[name] = KPI_Stats(
+        name=name,
+        data_location=filepath,
+        schema_file="./examples/test_kpi_definitions.yaml",
+    )
+
+    name = "SinksAnaSum"
+    kpi[name] = KPI_Stats(
+        name=name,
+        data_location=filepath,
+        schema_file="./examples/test_kpi_definitions.yaml",
+    )
+
+    name = "PerfAnaSum"
+    kpi[name] = KPI_Stats(
+        name=name,
+        data_location=filepath,
+        schema_file="./examples/test_kpi_definitions.yaml",
+    )
+
+    name = "RouteAnaSum"
+    kpi[name] = KPI_Stats(
+        name=name,
+        data_location=filepath,
+        schema_file="./examples/test_kpi_definitions.yaml",
+    )
+
+    name = "BulkDataLat"
+    kpi[name] = KPI_Stats(
+        name=name,
+        data_location=filepath,
+        schema_file="./examples/test_kpi_definitions.yaml",
+    )
+
+    name = "McastLat"
+    kpi[name] = KPI_Stats(
+        name=name,
+        data_location=filepath,
+        schema_file="./examples/test_kpi_definitions.yaml",
+    )
+
+    name = "DLn2nLat"
+    kpi[name] = KPI_Stats(
+        name=name,
+        data_location=filepath,
+        schema_file="./examples/test_kpi_definitions.yaml",
+    )
+
+    name = "ULtotalLat"
+    kpi[name] = KPI_Stats(
+        name=name,
+        data_location=filepath,
+        schema_file="./examples/test_kpi_definitions.yaml",
+    )
+
+    name = "ULperHopLat"
+    kpi[name] = KPI_Stats(
+        name=name,
+        data_location=filepath,
+        schema_file="./examples/test_kpi_definitions.yaml",
+    )
+
+    # section = kpi_stats.retrieve()
+    # df = section.apply(
+    #    lambda x: logger.info(json.loads(x.to_json(date_format="iso"))), axis=1
+    # )
