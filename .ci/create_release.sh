003a7d704268f3b93ccea8f1741daf7a51a22340
Pedro Figueiredo e Silva <PFigs@users.noreply.github.com>
2020-01-15 08:24:15 +0200

    Helper script for release and changelog creation (#206)

--- .ci/create_release.sh
+++ .ci/create_release.sh
@@ -4,11 +4,48 @@ set -e
 
 GIT_TAG="${1}"
 GIT_MESSAGE="${2:-"release ${GIT_TAG}"}"
+PY_VERSION_PATH="wirepas_backend_client/__about__.py"
+GH_TOKEN=${GH_TOKEN}
 
-echo "creating changelog..."
-github_changelog_generator -t "${GH_TOKEN}" --no-unreleased
+# update the python version to match the provided tag
+function update_version
+{
+    echo "updating and creating commit for release ${GIT_TAG}..."
+    sed -i "/__version__/d" "${PY_VERSION_PATH}"
+    echo "__version__=\"${GIT_TAG}\"" >> "${PY_VERSION_PATH}"
 
-echo "creating ${GIT_TAG}: ${GIT_MESSAGE}"
-git tag -m "${GIT_MESSAGE}" "${GIT_TAG}"
+    git add "${PY_VERSION_PATH}"
+    git commit -m "${GIT_MESSAGE}"
+}
 
-echo "done"
+# updates the changelog up to the current tag and commits changes
+function update_changelog
+{
+    echo "creating changelog..."
+    github_changelog_generator -t "${GH_TOKEN}" --no-unreleased
+
+    git add CHANGELOG.md
+    git commit --amend
+}
+
+# creates the tag
+function create_tag
+{
+    echo "creating ${GIT_TAG}: ${GIT_MESSAGE}"
+    git tag -m "${GIT_MESSAGE}" "${GIT_TAG}"
+}
+
+function _main
+{
+    if [[ ! -z "${GH_TOKEN}" ]]
+    then
+        update_version
+        create_tag
+        update_changelog
+        echo "done"
+    else
+        echo "please provide GH token for changelog generation"
+    fi
+}
+
+_main
